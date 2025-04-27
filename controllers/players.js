84f930496ee07921d170aacbc8c57ac9f9dbb212
marcophilip <marcophilipagapito@gmail.com>
2025-04-27 03:31:01 -0700

    player profile, player edit form route and delete a player function defined in players.js. ejs files for the three are in the works while the edit and delete a player buttons is written as a start.

--- controllers/players.js
+++ controllers/players.js
@@ -13,16 +13,40 @@ router.get('/new', async (req, res) => {
     try {
         // look for user and team from req.params
         const { userId, teamId } = req.params;
-
+        // find the team and user by ids from DB 
         const user = await User.findById(userId);
         const team = await Team.findById(teamId);
-        // render players/new.ejs. pass the userId and found team
+        // render players/new.ejs. pass the user and found team
         res.render('players/new.ejs', {
             user: user,
             team: team,
         })
     } catch (error) {
         //if error occurs, log and then redirect back to home
+        console.log(error);
+        res.redirect('/');
+    }
+});
+
+// GET /:playerId (Show a player profile)
+router.get('/:playerId', async (req, res) => {
+    try {
+        // get the ids from req.params
+        const { userId, teamId, playerId } = req.params;
+        // find ids from DB
+        const user = await User.findById(userId);
+        const team = await Team.findById(teamId);
+        const player = await Player.findById(playerId);
+
+        // render players/show.ejs and pass data if needed
+        res.render('players/show.ejs', {
+            user: user,
+            team: team,
+            player: player,
+            currentUser: req.session.user,
+        });
+    } catch (error) {
+        // if any errors, log it and redirect back home 
         console.log(error);
         res.redirect('/');
     }
@@ -59,9 +83,20 @@ router.post('/', async (req, res) => {
 });
 
 // GET /:playerId/edit (Form to edit a player)
-router.get('/', async (req, res) => {
+router.get('/:playerId/edit', async (req, res) => {
     try {
-        
+        // get the the ids from the req.params
+        const { userId, teamId, playerId } = req.params;
+        // find the user, team and player from DB
+        const user = await User.findById(userId);
+        const team = await Team.findById(teamId);
+        const player = await Player.findById(playerId);
+        // render players.edit ejs and pass the user, team and player datas
+        res.render('players/edit.ejs', {
+            user: user,
+            team: team,
+            player: player,
+        })
     } catch (error) {
         //if error occurs, log and then redirect back to home
         console.log(error);
@@ -70,7 +105,7 @@ router.get('/', async (req, res) => {
 });
 
 // PUT /:playerId (Edit a player)
-router.put('/', async (req, res) => {
+router.put('/:playerId', async (req, res) => {
     try {
         
     } catch (error) {
@@ -81,9 +116,18 @@ router.put('/', async (req, res) => {
 });
 
 // DELETE /:playerId (Delete a player)
-router.delete('/', async (req, res) => {
+router.delete('/:playerId', async (req, res) => {
     try {
-        
+         // get the the ids from the req.params
+         const { userId, teamId, playerId } = req.params;
+         // remove player from the team array using $pull method
+         await Team.findByIdAndUpdate(teamId, {
+            $pull: {players: playerId}
+         });
+         // delete the player data
+         await Player.findByIdAndDelete(playerId);
+         // res.redirect to the team id showpage
+         res.redirect(`/users/${userId}/teams/${teamId}`);
     } catch (error) {
         //if error occurs, log and then redirect back to home
         console.log(error);
